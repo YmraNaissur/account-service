d65522905834b41be3dc06a97e9c85e306c10e4f
max.karavaev <daily.spb@yandex.ru>
2022-01-21 01:18:33 +0300

    test: rewrite tests to use H2 database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO account (id, accountNumber, accountStatus, balance, customerName, customerNumber)
+VALUES (1, 1, 0, 550.78, 'John Connor', 1);
+INSERT INTO account (id, accountNumber, accountStatus, balance, customerName, customerNumber)
+VALUES (2, 2, 0, 2389.32, 'Keith Flint', 2);
+INSERT INTO account (id, accountNumber, accountStatus, balance, customerName, customerNumber)
+VALUES (3, 3, 0, 3499.12, 'Billie Piper', 3);
+INSERT INTO account (id, accountNumber, accountStatus, balance, customerName, customerNumber)
+VALUES (4, 4, 0, 890.54, 'Steve Jobs', 4);
+INSERT INTO account (id, accountNumber, accountStatus, balance, customerName, customerNumber)
+VALUES (5, 5, 0, 1298.34, 'Brian Holland', 5);
--- src/test/java/quarkus/AccountResourceTest.java
+++ src/test/java/quarkus/AccountResourceTest.java
@@ -1,5 +1,7 @@
 package quarkus;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -12,58 +14,48 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AccountResourceTest {
-/*    @Test
+    @Test
     @Order(1)
     void testRetrieveAll() {
         Response result = given()
                 .when().get("/accounts")
                 .then().statusCode(200)
                 .body(
-                        containsString("Max Karavaev"),
-                        containsString("Alexander Korneev"),
-                        containsString("Artem Zhvikov")
+                        containsString("John Connor"),
+                        containsString("Steve Jobs"),
+                        containsString("Brian Holland")
                 )
                 .extract()
                 .response();
 
         List<Account> accounts = result.jsonPath().getList("$");
         assertThat(accounts, not(empty()));
-        assertThat(accounts, hasSize(3));
+        assertThat(accounts, hasSize(5));
     }
 
     @Test
     @Order(2)
-    void testGetAccount() {
-        Account account = given()
-                .when().get("/accounts/{accountNumber}", 1)
-                .then()
-                .statusCode(200)
-                .extract()
-                .as(Account.class);
-
-        assertThat(account.getAccountNumber(), equalTo(1L));
-        assertThat(account.getCustomerName(), equalTo("Max Karavaev"));
-        assertThat(account.getBalance(), equalTo(new BigDecimal("678000")));
-        assertThat(account.getAccountStatus(), equalTo(AccountStatus.OPEN));
-    }
-
-    @Test
-    @Order(3)
     void testCreateAccount() {
-        Account newAccount = new Account(5L, 12L, "Ruslan Volovik", new BigDecimal("48500"));
+        Account newAccount = new Account();
+        newAccount.setAccountNumber(10L);
+        newAccount.setCustomerNumber(10L);
+        newAccount.setCustomerName("Jason Biggs");
 
         Account returnedAccount = given()
                 .contentType(ContentType.JSON)
                 .body(newAccount)
                 .when().post("/accounts")
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 .extract()
                 .as(Account.class);
 
@@ -75,16 +67,76 @@ class AccountResourceTest {
                 .then()
                 .statusCode(200)
                 .body(
-                        containsString("Max Karavaev"),
-                        containsString("Alexander Korneev"),
-                        containsString("Artem Zhvikov"),
-                        containsString("Ruslan Volovik")
+                        containsString("John Connor"),
+                        containsString("Steve Jobs"),
+                        containsString("Brian Holland"),
+                        containsString("Jason Biggs")
                 )
                 .extract()
                 .response();
 
         List<Account> accounts = result.jsonPath().getList("$");
         assertThat(accounts, not(empty()));
-        assertThat(accounts, hasSize(4));
-    }*/
+        assertThat(accounts, hasSize(6));
+    }
+
+    @Test
+    void testGetAccount() {
+        Account account = given()
+                .when().get("/accounts/{accountNumber}", 1)
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(Account.class);
+
+        assertThat(account.getAccountNumber(), equalTo(1L));
+        assertThat(account.getCustomerName(), equalTo("John Connor"));
+        assertThat(account.getBalance(), equalTo(new BigDecimal("550.78")));
+        assertThat(account.getAccountStatus(), equalTo(AccountStatus.OPEN));
+    }
+
+    @Test
+    void closeAccountTest() {
+        Response result = given()
+                .when().get("/accounts/close/{accountNumber}", 1)
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        String status = result.jsonPath().getString("accountStatus");
+        Double balance = result.jsonPath().getDouble("balance");
+        assertEquals("CLOSED", status);
+        assertEquals(0.0, balance);
+    }
+
+    @Test
+    void withdrawTest() {
+        Response result = given()
+                .when().queryParam("accountNumber", 2)
+                .queryParam("value", 389.32)
+                .get("/accounts/withdraw")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        Double balance = result.jsonPath().getDouble("balance");
+        assertEquals(2000.0, balance);
+    }
+
+    @Test
+    void depositTest() {
+        Response result = given()
+                .when().queryParam("accountNumber", 3)
+                .queryParam("value", 500.88)
+                .get("/accounts/deposit")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        Double balance = result.jsonPath().getDouble("balance");
+        assertEquals(4000.0, balance);
+    }
 }
